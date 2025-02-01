c496b3c059cbcc47bb38a0a35d923caea0dfa274
Ivan Bazaga <ivan.cpweb@gmail.com>
2025-02-01 12:01:08 +0000

    Actualización del proyecto para exportarlo a otra máquina

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,15 +33,20 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
 }
 
 dependencies {
 
+    implementation(libs.androidx.material)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.material)
+    implementation(libs.support.annotations)
+    implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/vanzaga/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/vanzaga/miprimeraapp/MainActivity.kt
@@ -1,20 +1,75 @@
 package com.vanzaga.miprimeraapp
 
+import android.app.ActivityManager.AppTask
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnAddTask: Button
+    lateinit var etTask: EditText
+    lateinit var rvTasks: RecyclerView
+    lateinit var adapter: TaskAdapter
+
+    var tasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initUi()
+
     }
+
+    private fun initUi() {
+        initView()
+        initListeners()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        adapter = TaskAdapter(tasks)
+        rvTasks.adapter = adapter
+
+    }
+
+    private fun initListeners() {
+        btnAddTask.setOnClickListener{
+            addTask()
+        }
+    }
+
+    // Esta función se encarga de agregar una tarea a la lista de tareas
+    private fun addTask() {
+        val taskToAdd: String = etTask.text.toString()
+        tasks.add(taskToAdd)
+        adapter.notifyDataSetChanged()
+        etTask.text.clear()
+    }
+
+    private fun initView() {
+        btnAddTask = findViewById(R.id.btnAddTask)
+        etTask = findViewById(R.id.etTask)
+        rvTasks = findViewById(R.id.rvTasks)
+    }
+
 }
